eff9dbd53a35b3bceec567558adfa527bc5d92d3
debeerphilip63-prog <debeerphilip63@gmail.com>
2026-01-28 10:13:53 +0200

    Make /admin dynamic to avoid prerender on build

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,6 +1,7 @@
 import AppShell from "@/components/layout/AppShell";
 import AdminClient from "@/components/admin/AdminClient";
 
+export const dynamic = "force-dynamic";
 export default function AdminPage() {
   return (
     <AppShell active="admin">
